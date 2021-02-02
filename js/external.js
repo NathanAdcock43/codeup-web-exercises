e9d379c31361d15c3b34aef00cb47a8669e866e5
Nathan Adcock <nathan.adcock43@gmail.com>
2021-02-02 14:30:57 -0600

    Feat: added question three to exercises

--- js/external.js
+++ js/external.js
@@ -4,7 +4,7 @@ var favColor = prompt('What is your favorite Color?');
 console.log("your favorite color is " + favColor);
 alert("Great " + favColor + " is my favorite color too!");
 
-
+// question 1
 var mer = prompt("How many days did you keep the Little Mermaid rental?");
 var bear = prompt("How many days did you keep the Brother Bear rental?");
 var herc = prompt("How many days did you keep the Hercules rental?");
@@ -14,6 +14,7 @@ rentTotal = Number(mer)  + Number(bear) + Number(herc);
 
 prompt("Wow, You spent a total of " + (rentTotal) + " dollars on rentals");
 
+//question 2
 var gooHours;
 gooHours = prompt("How many hours did you work at Google?");
 var gooPay;
@@ -31,6 +32,18 @@ var totalPay;
 totalPay= (gooHours * gooPay) + (amaHours * amaPay) + (faceHours * facePay);
 prompt("Wow, You got paid a total of " + (totalPay) + " dollars from your jobs! You are rich!");
 
+//question 3
+var room = confirm("Is there still room available in the class?");
+var sch = confirm("Is there still room on your schedule?");
+var cantakeclass = room && sch;
+if (cantakeclass === true) {
+    alert("Congrats you can enroll");
+}
+else{
+    alert("Sorry, you will have to wait till next time");
+}
+
+//question 4
 var items;
 items = prompt("How many things did you end up buying?");
 var premium;
@@ -38,5 +51,11 @@ premium = confirm("Are you a premium member?");
 var expire;
 expire = confirm("Is your offer expired?");
 var offercanapply = (items >= 2 || premium === true) && !expire === true;
-if (offercanapply === true) {alert("Congrats, You are eligible for this offer")}
+if (offercanapply === true) {
+    alert("Congrats, You are eligible for this offer");
+    }
+    else{
+    alert("Sorry, this offer is not available");
+    }
+
 
