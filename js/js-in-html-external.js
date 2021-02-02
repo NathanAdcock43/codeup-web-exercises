3dd894b36a1f214ccbc42984320689b49c5d2092
Nathan Adcock <nathan.adcock43@gmail.com>
2021-02-02 11:20:01 -0600

    Feat: Testing Alerts, Confirms, and Prompts

--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -2,4 +2,13 @@
 console.log("Hello, is this working");
 var name = "Nathan";
 console.log("Hello, " + name);
+//alert
+alert("Hey Where do you think you are going?!");
+//confirm
+var confirmLeave = confirm("So you are just going to leave?");
+console.log("confirmLeave" + confirmLeave);
 
+//prompt
+
+var favoriteColor = prompt('What is your favorite Color?')
+console.log("your favorite color is " + favoriteColor)
