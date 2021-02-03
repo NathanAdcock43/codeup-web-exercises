116b82c652406efd2e7bb6aeff0521b010b782e9
Nathan Adcock <nathan.adcock43@gmail.com>
2021-02-03 12:32:08 -0600

    Feat: Functions Lesson Part 1

---
+++ js/FuctionsLessons.js
@@ -0,0 +1,42 @@
+//function someFunctionNoParameter(){
+    //inside of here -we tell our function to do something
+   // return alert("Hello, How are you?");
+//}
+
+//someFunctionNoParameter()
+
+// function someFunctionWithParameter(name){
+//     //indside of here - we tell our function to do something
+//     return alert("Hello there " + name);
+// }
+// someFunctionWithParameter(Nathan);
+//
+//
+// //A function that returns a value >> assign that value to a variable
+//
+// function increment (num){
+//     return num + 1;
+// }
+//console.log(increment(num: 1));
+
+//var result = increment (num: 1);
+//console.log(result);
+
+//var four = increment(num: 3)
+//var five = increment(increment(num:3));
+//var six = increment(increment(increment(num:3)))
+
+//console.log(four)
+//console.log(five)
+//console.log(six)
+
+// function numberExperiment (num){
+//     return num
+// }
+// console.log(numberExperiment(1, 2));
+//
+var anonomousTest = function(){
+    return "beep, beep";
+}
+
+console.log(anonomousTest());
