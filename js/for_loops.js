f873b6cf70c790a4bdcb11ea366adabc94e6f71f
Nathan Adcock <nathan.adcock43@gmail.com>
2021-02-11 16:56:26 -0600

    Feat: Loop Exercises JS

---
+++ js/for_loops.js
@@ -0,0 +1,93 @@
+// Loops Exercises
+
+
+
+//Exercise 1
+
+// Create a function named showMultiplicationTable that accepts a number and console.logs
+// the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+function showMultiplicationTable(x){
+    for (var i = 1; i <= 10; i++) {
+        console.log(x + " X " + i + " = " + (i * x));
+    }
+}
+
+// showMultiplicationTable(10);
+
+
+function MultiplicationTableOneToTen (){
+    for (var i = 1; i <= 10; i++) {
+        console.log(showMultiplicationTable(i));
+    }
+}
+
+    MultiplicationTableOneToTen()
+
+//Exercise 2
+
+// Use a for loop and the code from the previous lessons to generate 10 random numbers
+// between 20 and 200 and output to the console whether each number is odd or even.
+// var randomNumber = Math.floor((Math.random() * 3) + 1);
+// for (var i = 0; i >=10 ; i++) {
+//
+// }
+
+
+//Exercise 3
+
+// Create a for loop that uses console.log to create the output shown below.
+//
+//
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+
+
+
+
+//Exercise 4
+
+// Create a for loop that uses console.log to create the output shown below.
+//
+//
+// 100
+// 95
+// 90
+// 85
+// 80
+// 75
+// 70
+// 65
+// 60
+// 55
+// 50
+// 45
+// 40
+// 35
+// 30
+// 25
+// 20
+// 15
+// 10
+// 5
+
+
+
+
+
+
+
+
+
+
+
+
+
