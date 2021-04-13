fe3aaac5215a5e8f466611a987beb1b6eae25c41
Nathan Adcock <nathan.adcock43@gmail.com>
2021-04-13 15:14:02 -0500

    feat: Map preject

--- js/VideoBookMarkingNotes.js
+++ js/VideoBookMarkingNotes.js
@@ -1,6 +1,53 @@
 "use strict";
 
-// (function() {
-//     var searchVid = document.getElementsByClassName("userVid");
-//     searchVid.setAttribute("source", "#userURL");
-// })();
+var URLInput = document.forms. ;
+
+(function() {
+    // var videoId = #userURL;
+    var searchVid = document.getElementById("videoWindow");
+    searchVid.setAttribute("src", "https://www.youtube.com/embed/" + videoID);
+})();
+
+
+
+// function preg_match (regex, str) {
+//     if (new RegExp(regex).test(str)){
+//         return regex.exec(str);
+//     }
+//     return false;
+// }
+//
+// function parseYouTubeTokenByUri(string $url): ?string
+//     {
+//         if (strncmp($url, 'user/', 5) === 0) { // 1.
+//     return null;
+// }
+//
+// if (preg_match('/^[a-zA-Z0-9\-\_]{11}$/', $url)) { // 2.
+//     return $url;
+// }
+//
+// if (preg_match('/(?:watch\?v=|v\/|embed\/|ytscreeningroom\?v=|\?v=|\?vi=|e\/|watch\?.*vi?=|\?feature=[a-z_]*&v=|vi\/)([a-zA-Z0-9\-\_]{11})/', $url, $regularMatch)) { // 3.
+//     return $regularMatch[1];
+// }
+//
+// if (preg_match('/([a-zA-Z0-9\-\_]{11})(?:\?[a-z]|\&[a-z])/', $url, $organicParametersMatch)) { // 4.
+//     return $organicParametersMatch[1];
+// }
+//
+// if (preg_match('/u\/1\/([a-zA-Z0-9\-\_]{11})(?:\?rel=0)?$/', $url)) { // 5.
+//     return null; // 5. User channel without token.
+// }
+//
+// if (preg_match('/(?:watch%3Fv%3D|watch\?v%3D)([a-zA-Z0-9\-\_]{11})[%&]/', $url, $urlEncoded)) { // 6.
+//     return $urlEncoded[1];
+// }
+//
+// // 7. Rules for special cases
+// if (preg_match('/watchv=([a-zA-Z0-9\-\_]{11})&list=/', $url, $special1)) {
+//     return $special1[1];
+// }
+//
+// return null;
+// }
+//
