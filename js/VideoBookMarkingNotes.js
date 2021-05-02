d40003ea52be205b3138d7c57ae57b4c52d5c276
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-02 16:03:18 -0500

    feat: trying to figureout getCurrentTime() on pause

--- js/VideoBookMarkingNotes.js
+++ js/VideoBookMarkingNotes.js
@@ -17,87 +17,47 @@ $(document).ready(function () {
     });
 
 
-let youtubeId = getYoutubeVideoID($('#userInputtedUrl').val());
-let time_total;
-let timeout_setter;
-let player;
-// var tag = document.createElement("script");//This code loads the IFrame Player API code asynchronously
-//
-// tag.src = "https://www.youtube.com/iframe_api";
-// var firstScriptTag = document.getElementsByTagName("script")[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-//
-// //This function creates an <iframe> (and YouTube player) OR uses the iframe if it exists at the "player" element after the API code downloads
-function onYouTubeIframeAPIReady()
-{
-    player = new playerjs.Player("videoPlayer",
-        {
-            width: "900px",
-            height: "500px",
-            videoId: youtubeId,
-            events:
-                {
-                    "onReady": onPlayerReady,
-                    "onStateChange": onPlayerStateChange
-                }
-        });
-}
+// let youtubeId = getYoutubeVideoID($('#userInputtedUrl').val());
 
-//
-// //new code to help load the player
-//The API will call this function when the video player is ready
-function onPlayerReady(event)
-{
-    event.target.playVideo();
-    time_total  = convert_to_mins_and_secs(player.getDuration(), 1);
-    loopy();
-}
-//
-function loopy()
-{
-    var current_time = convert_to_mins_and_secs(player.getCurrentTime(), 0);
-    document.getElementById("progress-bar").style.width = (player.getCurrentTime()/player.getDuration())*100+"%";
-    console.log( current_time + " / " + time_total);
-    timeout_setter = setTimeout(loopy, 1000);
-}
-//
-function convert_to_mins_and_secs(seconds, minus1)
-{
-    var mins    = (seconds>=60) ?Math.round(seconds/60):0;
-    var secs    = (seconds%60!=0) ?Math.round(seconds%60):0;
-    var secs    = (minus1==true) ?(secs-1):secs; //Youtube always displays 1 sec less than its duration time!!! Then we have to set minus1 flag to true for converting player.getDuration()
-    var time    = mins + ":" + ((secs<10)?"0"+secs:secs);
-    return time;
-}
-//
-// 5. The API calls this function when the player's state changes
-function onPlayerStateChange(event)
-{
-    if (event.data == playerjs.PlayerState.ENDED)
-    {
-        console.log("END!");
-        clearTimeout(timeout_setter);
-    }
-    else
-    {
-        console.log(event.data);
-    }
-}
+    let videoPlayer = document.getElementById("videoPlayer");
+
+    videoPlayer.addEventListener("playing", pausing_function);
+        let timer;
+    var pausing_function_1 = function() {
+        if (the video player is paused) {
+            console.log(videoPlayer.getCurrentTime());
+
+
+            // remove the event listener after you paused the playback
+            yid.removeEventListener("playing", pausing_function);
+        }
+    };
 
 });
 
-//// 5. TODO: The API calls this function when the player's state changes.
-
-//       //    The function indicates that when playing a video (state=1),
-//       //    the player should play for six seconds and then stop.
-//       var done = false;
-//       function onPlayerStateChange(event) {
-//         if (event.data == YT.PlayerState.PLAYING && !done) {
-//           setTimeout(stopVideo, 6000);
-//           done = true;
-//         }
-//       }
-//       function stopVideo() {
-//         player.stopVideo();
-//       }
-
+// Use jQuery to find all the embedly iframes on the page.
+// $('iframe.embedly-embed').each(function(){
+//     // initialize the player.
+//     var player = new playerjs.Player(this);
+//
+//     // Wait for the player to be ready.
+//     player.on('ready', function(){
+//
+//         // Listen to the play event.
+//         player.on('play', function(){
+//             // Tell Google analytics that a video was played.
+//             window.ga('send', 'event', 'Video', 'Play');
+//         });
+// //
+//         //autoplay the video.
+//         player.play();
+//     });
+//     player.getCurrentTime(function(seconds){
+//         console.log('Current Time: '+seconds);
+//     });
+// });
+//
+//     var iframe = document.querySelector('iframe.embedly-embed');
+//
+//     var player = new playerjs.Player(iframe);
+//
